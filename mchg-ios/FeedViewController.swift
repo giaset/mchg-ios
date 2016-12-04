924a5af5a6918856c76f3962d07d5814923250c8
Gianni Settino <g@miltonandparc.com>
2016-12-04 17:44:31 -0500

    fuck storyboards

---
+++ mchg-ios/FeedViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FeedViewController.swift
+//  mchg-ios
+//
+//  Created by Gianni Settino on 2016-12-04.
+//  Copyright © 2016 Milton and Parc. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.red
+    }
+}
