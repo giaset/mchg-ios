f250051693a4e787e6002ab83365056dca20ae16
Gianni Settino <g@miltonandparc.com>
2016-12-04 17:35:31 -0500

    initial commit

---
+++ mchg-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mchg-ios
+//
+//  Created by Gianni Settino on 2016-12-04.
+//  Copyright © 2016 Milton and Parc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
