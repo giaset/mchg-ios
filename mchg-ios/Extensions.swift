523390778acf5bfd9404cdc56e3cb65f89d0f4c9
Gianni Settino <g@miltonandparc.com>
2016-12-10 10:20:48 -0500

    adds uicolor-from-hex initializer

--- mchg-ios/Extensions.swift
+++ mchg-ios/Extensions.swift
@@ -8,9 +8,44 @@
 
 import UIKit
 
+extension UIColor {
+    
+    convenience init(hexString: String) {
+        let hex = hexString.replacingOccurrences(of: "#", with: "")
+        let length = hex.characters.count
+        
+        let scanner = Scanner(string:hex)
+        var hexValue: CUnsignedLongLong = 0
+        
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        var alpha: CGFloat = 1
+        
+        if scanner.scanHexInt64(&hexValue) {
+            if length == 6 {
+                red = CGFloat(Double((hexValue & 0xFF0000) >> 16) / 255)
+                green = CGFloat(Double((hexValue & 0x00FF00) >> 8) / 255)
+                blue = CGFloat(Double(hexValue & 0x0000FF) / 255)
+            } else if length == 8 {
+                red = CGFloat(Double((hexValue & 0xFF000000) >> 24) / 255)
+                green = CGFloat(Double((hexValue & 0x00FF0000) >> 16) / 255)
+                blue = CGFloat(Double((hexValue & 0x0000FF00) >> 8) / 255)
+                alpha = CGFloat(Double(hexValue & 0x000000FF) / 255)
+            } else {
+                print("invalid hex string")
+            }
+        } else {
+            print("hex scan error")
+        }
+        
+        self.init(red: red, green: green, blue: blue, alpha: alpha)
+    }
+}
+
 extension UIImage {
     
-    static let grailedPlaceholderImage = UIImage.imageWithColor(UIColor(red: 0.93, green: 0.93, blue: 0.93, alpha: 1))
+    static let grailedPlaceholderImage = UIImage.imageWithColor(UIColor(hexString: "eeeeee"))
     
     class func imageWithColor(_ color: UIColor) -> UIImage? {
         let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
